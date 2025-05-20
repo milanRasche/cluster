d0962a2febff2f9c897fda554823668cde2073fc
Milan Rasche <TheSlipperyTrout@gmail.com>
2025-05-20 11:45:40 +0200

    .gitignore change

---
+++ Cluster/services/Auth.API/Controllers/RunnerAuthController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Auth.API.Controllers
+{
+    public class RunnerAuthController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
