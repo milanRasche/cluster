8e9e160b75bcc4777bf82eae583d362b739807a4
Milan <theslipperytrout@gmail.com>
2025-06-08 00:42:29 +0200

    Added tests and adjusted ci

---
+++ Cluster/service-tests/Auth.API.Tests/MigrationFileTests.cs
@@ -0,0 +1,32 @@
+﻿using Auth.API.Data;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Auth.API.Tests
+{
+    public class MigrationFileTests
+    {
+        private const string ConnectionString = "Server=localhost,1433;Database=MigrationTestDb;User Id=sa;Password=Your_password123;TrustServerCertificate=True;";
+
+        [Fact]
+        public async Task CanApplyMigrationsToSqlServer()
+        {
+            var options = new DbContextOptionsBuilder<ApplicationDbContext>()
+                .UseSqlServer(ConnectionString)
+                .Options;
+
+            using var context = new ApplicationDbContext(options);
+
+            await context.Database.EnsureDeletedAsync();
+            await context.Database.MigrateAsync();
+
+            var canConnect = await context.Database.CanConnectAsync();
+            Assert.True(canConnect);
+        }
+    }
+}
